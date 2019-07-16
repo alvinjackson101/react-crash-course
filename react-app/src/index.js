1203eaf58a76d1d5f69295ce22105672b4328c2b
Alvin Jackson <alvinjackson@Alvins-MBP.lan1>
2019-07-16 06:52:43 -0500

    trying to push website live

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const element = <h1>Hello World</h1>;
-vonsole.log(element);
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
