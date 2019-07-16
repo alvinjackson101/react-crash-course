e75e82506cff1afd682486bd81b66776236e7027
Alvin Jackson <alvinjackson@Alvins-MBP.attlocal.net>
2019-07-15 21:26:27 -0500

    updated

--- react-app/counter-app/src/components/counter.jsx
+++ react-app/counter-app/src/components/counter.jsx
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: this.props.value
-  };
-
   styles = {
     fontSize: 10,
     fontWeight: "bold"
-  };
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.value + 1 });
   };
 
   render() {
@@ -20,11 +11,17 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           // onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -32,13 +29,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
